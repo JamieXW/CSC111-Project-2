bd476ee2218d1e35028dabb398cb04ebc916c05f
rih0o <rihonch1i@gmail.com>
2025-03-26 23:49:28 -0400

    Update graph_builder.py

--- graph_builder.py
+++ graph_builder.py
@@ -7,7 +7,7 @@ from geopy.distance import geodesic
 from data_loader import load_neighbourhood_data, load_apartment_data
 
 class Apartment:
-    """An apartment.
+    """An apartment property.
 
     Instance Attributes:
         - beds: the number of beds in the apartment.
@@ -16,7 +16,11 @@ class Apartment:
         - coord: the coordinate of latitude and longtitude of the apartment.
 
     Representation Invariants:
-        -
+        - self.beds > 0
+        - self.price > 0
+        - len(self.address) > 0
+        - -90 <= self.coord[0] <= 90  # valid latitude
+        - -180 <= self.coord[1] <= 180  # valid longitude
     """
     beds: int
     address: str
@@ -24,26 +28,37 @@ class Apartment:
     coord: tuple[float, float]
 
     def __init__(self, beds: int, address: str, price: float, coord: tuple[float, float]):
+        """Initialize a new apartment with the given attributes.
+        """
         self.beds = beds
         self.address = address
         self.price = price
         self.coord = coord
 
     def price_per_bed(self) -> float:
+        """Return the price per bedroom for this apartment.
+        """
         return self.price / self.beds
 
 
 class Area:
-    """An area in Toronto.
+    """A neighborhood area in Toronto.
 
     Instance Attributes:
         - name: the name of the area.
         - assault_rate: the assault rate in the area.
         - homicide_rate: the homicide rate in the area.
+        - theft_rate: the theft rate in the area.
         - coord: the coordinate of latitude and longtitude of the area.
 
     Representation Invariants:
-        -
+        - len(self.name) > 0
+        - self.assault_rate >= 0
+        - self.homicide_rate >= 0
+        - self.theft_rate >= 0
+        - -90 <= self.coord[0] <= 90
+        - -180 <= self.coord[1] <= 180
+
     """
     name: str
     assault_rate: float
@@ -52,6 +67,7 @@ class Area:
     coord: tuple[float, float]
 
     def __init__(self, name: str, assault_rate: float, homicide_rate: float, theft_rate: float, coord: tuple[float, float]):
+        """Initialize a new area with the given attributes."""
         self.name = name
         self.assault_rate = assault_rate
         self.homicide_rate = homicide_rate
@@ -60,40 +76,46 @@ class Area:
         self.apartments: list[Apartment] = []
 
     def add_apartment(self, apt: Apartment) -> None:
+        """Add an apartment to this area's collection.
+        """
         self.apartments.append(apt)
 
     def avg_price(self) -> float:
+        """Calculate the average price of apartments in this area.
+        """
         if not self.apartments:
             return 0.0
         return sum(a.price for a in self.apartments) / len(self.apartments)
 
 
 class Graph:
-    """A graph of areas and apartments.
+    """A graph representation connecting apartments .
 
     Instance Attributes:
-        - areas:
-        - apartments:
-
-    Representation Invariants:
-        -
+        - areas: collection of neighborhood areas in the graph.
+        - apartments: collection of apartment properties in the graph.
     """
     areas: list[Area]
     apartments: list[Apartment]
 
     def __init__(self):
+        """Initialize an empty graph with no areas or apartments.
+        """
         self.areas = []
         self.apartments = []
 
     def add_area(self, area: Area) -> None:
+        """Add a neighborhood area to the graph.
+        """
         self.areas.append(area)
 
     def add_apartment(self, apt: Apartment) -> None:
+        """Add an apartment property to the graph.
+        """
         self.apartments.append(apt)
 
     def build_graph(self, neighbourhood_file: str, apartment_file: str) -> nx.Graph:
-        """
-        Build a graph from neighborhood and apartment data.
+        """Build a graph from neighborhood and apartment data.
         """
         # Load data
         neighbourhoods = load_neighbourhood_data(neighbourhood_file)
